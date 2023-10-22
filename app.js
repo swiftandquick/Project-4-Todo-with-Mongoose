721c9d9427e3247eec8e0e268ca91ca297bb71f4
swiftandquick <chenyMA16@gmail.com>
2023-10-22 15:58:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const path = require('path');
 const methodOverride = require('method-override');
 
@@ -22,7 +22,7 @@ mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(error => {
         console.log("Error!");
         console.log(error);
-    })
+    });
 
 app.set('view engine', 'ejs');
 
@@ -30,7 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(methodOverride('_method'));
 
@@ -74,5 +74,5 @@ app.all('*', (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('todo app live!! baby');
-})
+    console.log(`Listening on port ${port}.`);
+});
